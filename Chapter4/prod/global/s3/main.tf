b36e6781cfe73366a5cdcee6a3da35c968452699
devops4sachin <devops.sachin@gmail.com>
2019-02-12 14:28:20 +0530

    added comments

--- Chapter4/prod/global/s3/main.tf
+++ Chapter4/prod/global/s3/main.tf
@@ -20,3 +20,27 @@ resource "aws_s3_bucket" "terraform_state" {
 	}
 }
 
+# Dynamodb database for maintaing the locks.
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock-for-s3" {
+	name = "terraform-state-lock-s3"
+	hash_key = "LockID"
+	read_capacity = 20
+	write_capacity = 20
+
+	attribute {
+		name = "LockID"
+		type = "S"
+	}
+
+	tags {
+		Name = "DyanmoDB terraform State Lock"
+	}
+}
+
+########### following is the sequence
+#1 : First create the resource
+#2 : Create the bucket
+#3 : Then create the backend
+#4 : Add "dynamodb" database to the main.tf file
+#5 : edit "terraformlock.tf" to include the "dynamodb" database in the "s3" configuration
--- Chapter4/prod/global/s3/terraformlock.tf
+++ Chapter4/prod/global/s3/terraformlock.tf
@@ -9,19 +9,3 @@ terraform {
 	key = "prod/global/s3/terraform.tfstate"
 	}
 }
-
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock-for-s3" {
-	name = "terraform-state-lock-s3"
-	hash_key = "LockID"
-	read_capacity = 20
-	write_capacity = 20
-
-	attribute {
-		name = "LockID"
-		type = "S"
-	}
-
-	tags {
-		Name = "DyanmoDB terraform State Lock"
-	}
-}
